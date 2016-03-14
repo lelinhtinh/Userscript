9153aa7039641ae88e306228dfa21f69614d1838
Zzbaivong <baivong@users.noreply.github.com>
2016-03-15 00:38:13 +0700

    Hold the right mouse button to scroll

--- Javascript-Css_beautify.user.js
+++ Javascript-Css_beautify.user.js
@@ -2,8 +2,7 @@
 // @name        Javascript-css beautify
 // @namespace   http://devs.forumvi.com
 // @description Beautify and syntax highlight javascript/css source code
-// @include     *
-// @version     2.2.1
+// @version     2.3.0
 // @author      Zzbaivong
 // @resource    light https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/styles/github-gist.min.css
 // @resource    dark https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/styles/monokai-sublime.min.css
@@ -15,24 +14,66 @@
 // @grant       GM_getResourceText
 // ==/UserScript==
 
-(function() {
+(function () {
 
     'use strict';
 
     var theme = 'light', // light|dark
 
         url = window.top.location.pathname,
-        contenttype = document.contentType;
+        doc = document,
+        contenttype = doc.contentType;
+
+    function scrollByDragging(container) {
+
+        function mouseUp(e) {
+            if (e.which !== 3) return;
+
+            window.removeEventListener('mousemove', mouseMove, 0);
+            container.style.cursor = 'auto';
+        }
+
+        function mouseDown(e) {
+            if (e.which !== 3) return;
+
+            pos = {
+                x: e.clientX,
+                y: e.clientY
+            };
+
+            window.addEventListener('mousemove', mouseMove, 0);
+            container.style.cursor = 'move';
+        }
+
+        function mouseMove(e) {
+            container.scrollLeft -= (-pos.x + (pos.x = e.clientX));
+            container.scrollTop -= (-pos.y + (pos.y = e.clientY));
+        }
+
+        var pos = {
+            x: 0,
+            y: 0
+        };
+
+        if (container.clientWidth < container.scrollWidth || container.clientHeight < container.scrollHeight) {
+            container.addEventListener('mousedown', mouseDown, 0);
+            window.addEventListener('mouseup', mouseUp, false);
+            container.oncontextmenu = function (e) {
+                e.preventDefault();
+            };
+        }
+
+    }
 
     if (/^(application\/x-javascript|application\/javascript|application\/json|text\/css)$/.test(contenttype) || /.+\.(js|json|css)$/.test(url)) {
 
-        var output = document.getElementsByTagName('pre')[0],
+        var output = doc.getElementsByTagName('pre')[0],
             txt = output.textContent,
             lang = 'javascript',
             lines = 0,
             l = '';
 
-        GM_addStyle(GM_getResourceText(theme) + 'html,body,pre{margin:0;padding:0}.hljs{word-wrap:normal!important;white-space:pre!important;padding-left:4em;line-height:120%}.hljs::before{content:attr(data-lines);position:absolute;color:#d2d2d2;text-align:right;width:3.5em;left:-.5em;border-right:1px solid rgba(221, 221, 221, 0.36);padding-right:.5em}#scroll-x{position:fixed;right:0;top:0;width:120px;cursor:w-resize;z-index:999;background:transparent;bottom:0}');
+        GM_addStyle(GM_getResourceText(theme) + 'html,body,pre{margin:0;padding:0}.hljs{overflow:hidden;word-wrap:normal!important;white-space:pre!important;padding-left:4em;line-height:120%}.hljs::before{content:attr(data-lines);position:absolute;color:#d2d2d2;text-align:right;width:3.5em;left:-.5em;border-right:1px solid rgba(221, 221, 221, 0.36);padding-right:.5em}');
 
         if (contenttype === 'text/css' || /.+\.css$/.test(url)) {
             lang = 'css';
@@ -53,16 +94,9 @@
         }
 
         output.setAttribute('data-lines', l);
+        output.style.width = output.scrollWidth + 'px';
 
-        var node = document.createElement('DIV');
-        node.id = 'scroll-x';
-        document.body.appendChild(node);
-
-        node.onwheel = function(e) {
-            e.preventDefault();
-            output.scrollLeft += (e.deltaY * 10);
-            return false;
-        };
+        scrollByDragging(doc.body);
 
     }
 
--- View_source.user.js
+++ View_source.user.js
@@ -2,8 +2,7 @@
 // @name        viewsource
 // @namespace   devs.forumvi.com
 // @description Viewsource for Firefox, like Chrome
-// @include     *
-// @version     2.1.3
+// @version     2.3.0
 // @author      Zzbaivong
 // @resource    light https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/styles/github-gist.min.css
 // @resource    dark https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/styles/monokai-sublime.min.css
@@ -18,7 +17,7 @@
 // @grant       GM_registerMenuCommand
 // ==/UserScript==
 
-(function() {
+(function () {
 
     'use strict';
 
@@ -28,6 +27,47 @@
         urlpage = win.top.location.href,
         doc = document,
         content = doc.body;
+
+    function scrollByDragging(container) {
+
+        function mouseUp(e) {
+            if (e.which !== 3) return;
+
+            window.removeEventListener('mousemove', mouseMove, 0);
+            container.style.cursor = 'auto';
+        }
+
+        function mouseDown(e) {
+            if (e.which !== 3) return;
+
+            pos = {
+                x: e.clientX,
+                y: e.clientY
+            };
+
+            window.addEventListener('mousemove', mouseMove, 0);
+            container.style.cursor = 'move';
+        }
+
+        function mouseMove(e) {
+            container.scrollLeft -= (-pos.x + (pos.x = e.clientX));
+            container.scrollTop -= (-pos.y + (pos.y = e.clientY));
+        }
+
+        var pos = {
+            x: 0,
+            y: 0
+        };
+
+        if (container.clientWidth < container.scrollWidth || container.clientHeight < container.scrollHeight) {
+            container.addEventListener('mousedown', mouseDown, 0);
+            window.addEventListener('mouseup', mouseUp, false);
+            container.oncontextmenu = function (e) {
+                e.preventDefault();
+            };
+        }
+
+    }
 
     function removeEvents(ele, attr) {
         var events = 'onafterprint onbeforeprint onbeforeunload onerror onhashchange onload onmessage onoffline ononline onpagehide onpageshow onpopstate onresize  onstorage onunload onblur onchange oncontextmenu onfocus oninput oninvalid onreset onsearch onselect onsubmit onkeydown onkeypress onkeyup onclick ondblclick ondrag ondragend ondragenter ondragleave ondragover ondragstart ondrop onmousedown onmousemove onmouseout onmouseover onmouseup onmousewheel onscroll onwheel oncopy oncut onpaste onerror onshow ontoggle'.split(' '),
@@ -45,7 +85,7 @@
         GM_xmlhttpRequest({
             method: 'GET',
             url: urlpage,
-            onload: function(response) {
+            onload: function (response) {
 
                 removeEvents(win);
                 removeEvents(doc);
@@ -61,7 +101,7 @@
                 content.removeAttribute('style');
                 doc.title = 'view-source:' + urlpage;
 
-                GM_addStyle(GM_getResourceText(theme) + 'html,body,pre{margin:0;padding:0}.hljs{word-wrap:normal!important;white-space:pre!important;padding-left:4em;line-height:100%}.hljs::before{content:attr(data-lines);position:absolute;color:#d2d2d2;text-align:right;width:3.5em;left:-.5em;border-right:1px solid rgba(221, 221, 221, 0.36);padding-right:.5em}#scroll-x{position:fixed;right:0;top:0;width:120px;cursor:w-resize;z-index:999;background:transparent;bottom:0}');
+                GM_addStyle(GM_getResourceText(theme) + 'html,body,pre{margin:0;padding:0}.hljs{overflow:hidden;word-wrap:normal!important;white-space:pre!important;padding-left:4em;line-height:100%}.hljs::before{content:attr(data-lines);position:absolute;color:#d2d2d2;text-align:right;width:3.5em;left:-.5em;border-right:1px solid rgba(221, 221, 221, 0.36);padding-right:.5em}');
 
                 var output = doc.createElement('PRE');
                 output.setAttribute('class', 'xml');
@@ -79,16 +119,9 @@
                 }
 
                 output.setAttribute('data-lines', l);
+                output.style.width = output.scrollWidth + 'px';
 
-                var node = doc.createElement('DIV');
-                node.id = 'scroll-x';
-                content.appendChild(node);
-
-                node.onwheel = function(e) {
-                    e.preventDefault();
-                    output.scrollLeft += (e.deltaY * 10);
-                    return false;
-                };
+                scrollByDragging(content);
 
                 var attrUrl = doc.getElementsByClassName('hljs-attr');
                 for (var j = 0; j < attrUrl.length; j++) {
@@ -106,7 +139,7 @@
     GM_registerMenuCommand('View source', viewsource, 'm');
 
     if (doc.contentType === 'text/html' && doc.URL === urlpage) {
-        win.onkeydown = function(e) {
+        win.onkeydown = function (e) {
 
             // Ctrl + M
             if (e.which === 77 && e.ctrlKey) {
