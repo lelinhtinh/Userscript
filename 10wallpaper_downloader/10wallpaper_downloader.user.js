73d9006876f37ca0da0395aa9d5ca83689030c54
Zzbaivong <baivong@users.noreply.github.com>
2016-05-21 20:59:01 +0700

    10wallpaper.com

---
+++ 10wallpaper_downloader/10wallpaper_downloader.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @id           10wallpaper-downloader@baivong.github.io
+// @name         10wallpaper downloader
+// @namespace    http://baivong.github.io/
+// @version      1.0.0
+// @description  1-Click download on 10wallpaper. You should select the resolution before downloading.
+// @icon         http://i.imgur.com/08zfJez.png
+// @author       Zzbaivong
+// @license      MIT
+// @match        http://10wallpaper.com/*
+// @noframes
+// @supportURL   https://github.com/baivong/Userscript/issues
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Resolution config:
+    var resolution = ''; // Eg: 1920x1200, 1366x768, 1280x1024, ...
+
+
+    // Do not change the code below this line, unless you know how.
+    var list = document.querySelectorAll('a[href^="/view/"], a[href^="/list/"]');
+    if(!/\d+x\d+/.test(resolution)) resolution = screen.width + 'x' + screen.height;
+    [].forEach.call(list, function (item) {
+        var img = item.querySelector('img');
+        if (!img) return;
+
+        item.setAttribute('href', img.src.replace(/(\/|_)(small|medium)(\/|\.)/g, '$1' + resolution + '$3'));
+        item.setAttribute('download', '');
+    });
+})();
