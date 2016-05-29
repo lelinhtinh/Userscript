9632b4a2a936847603b0ad8f59040367256b1372
Zzbaivong <baivong@users.noreply.github.com>
2016-05-29 14:20:30 +0700

    Fixed #3

--- nHentai_downloader/nHentai_downloader.user.js
+++ nHentai_downloader/nHentai_downloader.user.js
@@ -3,14 +3,15 @@
 // @name         nHentai Downloader
 // @namespace    http://devs.forumvi.com
 // @description  Download manga on nHentai.net
-// @version      1.1.7
+// @version      1.2.0
 // @icon         http://i.imgur.com/FAsQ4vZ.png
 // @author       Zzbaivong
 // @license      MIT
 // @match        http://nhentai.net/g/*
+// @match        https://nhentai.net/g/*
 // @require      https://code.jquery.com/jquery-2.2.4.min.js
 // @require      https://greasyfork.org/scripts/19855-jszip/code/jszip.js?version=126859
-// @require      https://greasyfork.org/scripts/18532-filesaver/code/FileSaver.js?version=127839
+// @require      https://greasyfork.org/scripts/18532-filesaver/code/FileSaver.js?version=128198
 // @noframes
 // @connect      nhentai.net
 // @supportURL   https://github.com/baivong/Userscript/issues
@@ -66,7 +67,7 @@ jQuery(function($) {
         $download.html('<i class="fa fa-cog fa-spin"></i> Waiting...').css('backgroundColor', 'orange');
 
         $('.lazyload').each(function(i, v) {
-            images[i] = 'http:' + $(v).attr('data-src').replace('t.n', 'i.n').replace(/\/(\d+)t\./, '/$1.');
+            images[i] = location.protocol + $(v).attr('data-src').replace('t.n', 'i.n').replace(/\/(\d+)t\./, '/$1.');
         });
 
         total = images.length;
@@ -78,25 +79,27 @@ jQuery(function($) {
         });
 
         $.when.apply($, deferreds).done(function() {
-            var blob = zip.generate({
-                    type: 'blob'
-                }),
-                zipName = tit.split(' » ')[0].replace(/\s/g, '_') + '.zip';
+            zip.generateAsync({
+                type: 'blob'
+            }).then(function(blob) {
+                var zipName = tit.split(' » ')[0].replace(/\s/g, '_') + '.zip';
 
-            if (prevZip) {
-                window.URL.revokeObjectURL(prevZip);
-            }
-            prevZip = blob;
+                if (prevZip) {
+                    window.URL.revokeObjectURL(prevZip);
+                }
+                prevZip = blob;
 
-            $download.html('<i class="fa fa-check"></i> Complete').css('backgroundColor', 'green').attr({
-                href: window.URL.createObjectURL(prevZip),
-                download: zipName
+                $download.html('<i class="fa fa-check"></i> Complete').css('backgroundColor', 'green').attr({
+                    href: window.URL.createObjectURL(prevZip),
+                    download: zipName
+                });
+
+                saveAs(blob, zipName);
+
+                doc.title = '[⇓] ' + tit;
+            }, function(reason) {
+                 console.error(reason);
             });
-
-            saveAs(blob, zipName);
-
-            doc.title = '[⇓] ' + tit;
-
         }).fail(function(err) {
             $download.html('<i class="fa fa-exclamation"></i> Fail').css('backgroundColor', 'red');
             console.error(err);
