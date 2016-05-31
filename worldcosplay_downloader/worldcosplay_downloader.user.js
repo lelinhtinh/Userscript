93bc4eba94df161a2aaddd5dd759bea85c4688e8
Zzbaivong <baivong@users.noreply.github.com>
2016-05-31 23:27:52 +0700

    GM_download

--- worldcosplay_downloader/worldcosplay_downloader.user.js
+++ worldcosplay_downloader/worldcosplay_downloader.user.js
@@ -3,7 +3,7 @@
 // @name         Worldcosplay download
 // @namespace    http://devs.forumvi.com/
 // @description  Download photo(s) on worldcosplay.net
-// @version      1.1.8
+// @version      2.0.0
 // @icon         http://i.imgur.com/gJLjIzb.png
 // @author       Zzbaivong
 // @license      MIT
@@ -26,38 +26,50 @@
 // @match        http://worldcosplay.net/*/popular
 // @match        http://worldcosplay.net/*/ranking/good*
 // @require      https://code.jquery.com/jquery-2.2.4.min.js
-// @require      https://greasyfork.org/scripts/18532-filesaver/code/FileSaver.js?version=128198
+// @require      https://greasyfork.org/scripts/18532-filesaver/code/FileSaver.js?version=128649
 // @require      https://greasyfork.org/scripts/5392-waitforkeyelements/code/WaitForKeyElements.js?version=115012
 // @noframes
 // @connect      worldcosplay.net
 // @supportURL   https://github.com/baivong/Userscript/issues
 // @run-at       document-idle
 // @grant        GM_xmlhttpRequest
+// @grant        GM_download
 // ==/UserScript==
 
-(function($, window, document, undefined) {
+(function ($, window, document, undefined) {
     'use strict';
 
     window.URL = window.URL || window.webkitURL;
 
     function downloadPhoto(el, url) {
         var photoName = url.replace(/.*\//g, '');
-        GM_xmlhttpRequest({
-            method: 'GET',
-            url: url,
-            responseType: 'blob',
-            onload: function(response) {
-                var blob = response.response;
-                $(el).attr({
-                    href: window.URL.createObjectURL(blob),
-                    download: photoName
-                }).removeAttr('onclick');
-                saveAs(blob, photoName);
-            },
-            onerror: function(err) {
-                console.error(err);
-            }
-        });
+        if (typeof GM_download !== 'undefined') {
+            GM_download({
+                url: url,
+                name: photoName,
+                saveAs: false,
+                onerror: function (err) {
+                    console.error(err);
+                }
+            });
+        } else {
+            GM_xmlhttpRequest({
+                method: 'GET',
+                url: url,
+                responseType: 'blob',
+                onload: function (response) {
+                    var blob = response.response;
+                    $(el).attr({
+                        href: window.URL.createObjectURL(blob),
+                        download: photoName
+                    }).off('click');
+                    saveAs(blob, photoName);
+                },
+                onerror: function (err) {
+                    console.error(err);
+                }
+            });
+        }
     }
 
     if (/^(\/[a-z\-]+)?\/photo\/\d+$/.test(location.pathname)) {
@@ -65,9 +77,9 @@
         var $btn = $('<a>', {
             href: '#download',
             'class': 'download-this-photo',
-            html: '<div class="side_buttons" style="right: 220px;"><div class="like-this-photo button fave fa fa-download"><div class="effect-ripple"></div></div></div>'
+            html: '<div class="side_buttons" style="right: 250px;"><div class="like-this-photo button fave fa fa-download"><div class="effect-ripple"></div></div></div>'
         });
-        $btn.on('click', function(e) {
+        $btn.on('click', function (e) {
             e.preventDefault();
             e.stopPropagation();
             downloadPhoto(this, $('#photoContainer').find('.img').attr('src'));
@@ -76,15 +88,15 @@
 
     } else {
 
-        var addBtn = function() {
-            $('.preview').not('.added-download-btn').each(function() {
+        var addBtn = function () {
+            $('.preview').not('.added-download-btn').each(function () {
                 var $this = $(this),
                     $btn = $('<a>', {
                         href: '#download',
                         'class': 'download-this-photo',
                         html: '<div class="item likes" style="top: 50px;"><span class="like-this-photo"><i class="fa fa-download"></i><span class="effect-ripple"></span></span></div>'
                     });
-                $btn.on('click', function(e) {
+                $btn.on('click', function (e) {
                     e.preventDefault();
                     e.stopPropagation();
                     downloadPhoto(this, $this.find('.photo_img').css('backgroundImage').slice(5, -2).replace('sq300/', ''));
