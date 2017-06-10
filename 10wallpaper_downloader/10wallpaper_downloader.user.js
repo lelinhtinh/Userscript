55bddf1d1f7eb0dd7c1b870eb46f9984be60aab9
baivong <Zzbaivong@gmail.com>
2017-06-10 22:42:45 +0700

    Add right-click function

--- 10wallpaper_downloader/10wallpaper_downloader.user.js
+++ 10wallpaper_downloader/10wallpaper_downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         10wallpaper downloader
 // @namespace    http://baivong.github.io/
-// @version      1.0.0
+// @version      1.1.0
 // @description  1-Click download on 10wallpaper. You should select the resolution before downloading.
 // @icon         http://i.imgur.com/08zfJez.png
 // @author       Zzbaivong
@@ -22,12 +22,21 @@
 
     // Do not change the code below this line, unless you know how.
     var list = document.querySelectorAll('a[href^="/view/"], a[href^="/list/"]');
-    if(!/\d+x\d+/.test(resolution)) resolution = screen.width + 'x' + screen.height;
-    [].forEach.call(list, function (item) {
-        var img = item.querySelector('img');
-        if (!img) return;
+    if (!/\d+x\d+/.test(resolution)) resolution = screen.width + 'x' + screen.height;
+    [].forEach.call(list, function (link) {
+        var img = link.querySelector('img');
 
-        item.setAttribute('href', img.src.replace(/(\/|_)(small|medium)(\/|\.)/g, '$1' + resolution + '$3'));
-        item.setAttribute('download', '');
+        if (!img) return;
+        img = img.src.replace(/(\/|_)(small|medium)(\/|\.)/g, '$1' + resolution + '$3');
+
+        link.setAttribute('href', img);
+        link.setAttribute('download', '');
+
+        link.addEventListener('contextmenu', function (e) {
+            e.preventDefault();
+            window.open(img, '_blank');
+        });
+
+        link.setAttribute('title', 'Click to download this image\nRight Click to open in new tab');
     });
 })();
