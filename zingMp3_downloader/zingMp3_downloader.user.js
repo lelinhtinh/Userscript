7ea593310304261cde3da24c78c4dd877e79ac96
Lê Thành <baivong@users.noreply.github.com>
2017-08-29 10:54:44 +0700

    Update zingMp3_downloader.user.js

--- zingMp3_downloader/zingMp3_downloader.user.js
+++ zingMp3_downloader/zingMp3_downloader.user.js
@@ -2,7 +2,7 @@
 // @name         Download nhạc mp3 zing 320kbps
 // @namespace    baivong.download.mp3zing
 // @description  Download nhạc nhất lượng cao 320kbps tại mp3.zing.vn
-// @version      5.1.0
+// @version      5.2.0
 // @icon         http://i.imgur.com/PnF4UN2.png
 // @author       Zzbaivong
 // @license      MIT
@@ -12,15 +12,12 @@
 // @match        http://mp3.zing.vn/nghe-si/*
 // @match        http://mp3.zing.vn/tim-kiem/bai-hat.html?q=*
 // @match        http://mp3.zing.vn/bang-xep-hang/*
-// @match        https://linksvip.net/*
-// @require      https://greasyfork.org/scripts/5392-waitforkeyelements/code/WaitForKeyElements.js
 // @noframes
 // @supportURL   https://github.com/baivong/Userscript/issues
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
 
-/* global waitForKeyElements */
 (function ($, window, document) {
     'use strict';
 
@@ -34,80 +31,17 @@
     }
 
     function linksVip(songId) {
-        window.open('https://linksvip.net/?link=http://mp3.zing.vn/bai-hat/-/' + songId + '.html', 'mp3Zing320Helper_' + songId, 'height=10,width=10,left=0,top=99999,titlebar=no,toolbar=no,status=no,toolbar=no,scrollbars=no,menubar=no,location=no');
+        return 'https://linksvip.net/download/zingmp3.php?code=' + songId + '&q=320';
     }
 
-    function postMessage(type) {
-        var message = {
-            id: window.name.split('_')[1],
-            class: type
-        };
-        opener.postMessage(message, 'http://mp3.zing.vn');
+    addStyle('.bv-icon{background-image:url(http://static.mp3.zdn.vn/skins/zmp3-v4.1/images/icon.png)!important;background-repeat:no-repeat!important;background-position:-25px -2459px!important;}.bv-download{background-color:#721799!important;border-color:#721799!important;}.bv-download span{color:#fff!important;margin-left:8px!important;}.bv-disable,.bv-download:hover{background-color:#2c3e50!important;border-color:#2c3e50!important;}.bv-text{background-image:none!important;color:#fff!important;text-align:center!important;font-size:smaller!important;line-height:25px!important;}.bv-waiting{cursor:wait!important;background-color:#2980b9!important;border-color:#2980b9!important;}.bv-complete,.bv-complete:hover{background-color:#27ae60!important;border-color:#27ae60!important;}.bv-error,.bv-error:hover{background-color:#c0392b!important;border-color:#c0392b!important;}.bv-disable{cursor:not-allowed!important;opacity:0.4!important;}');
 
-        window.close();
-    }
-
-    function onMessage(e) {
-        if (e.origin !== 'https://linksvip.net') return;
-
-        $('.bv-download[data-id="' + e.data.id + '"]').removeClass('bv-waiting').addClass('bv-' + e.data.class);
-    }
-
-    if (location.host === 'mp3.zing.vn') {
-
-        window.addEventListener('message', onMessage, true);
-
-        addStyle('.bv-icon{background-image:url(http://static.mp3.zdn.vn/skins/zmp3-v4.1/images/icon.png)!important;background-repeat:no-repeat!important;background-position:-25px -2459px!important;}.bv-download{background-color:#721799!important;border-color:#721799!important;}.bv-download span{color:#fff!important;margin-left:8px!important;}.bv-disable,.bv-download:hover{background-color:#2c3e50!important;border-color:#2c3e50!important;}.bv-text{background-image:none!important;color:#fff!important;text-align:center!important;font-size:smaller!important;line-height:25px!important;}.bv-waiting{cursor:wait!important;background-color:#2980b9!important;border-color:#2980b9!important;}.bv-complete,.bv-complete:hover{background-color:#27ae60!important;border-color:#27ae60!important;}.bv-error,.bv-error:hover{background-color:#c0392b!important;border-color:#c0392b!important;}.bv-disable{cursor:not-allowed!important;opacity:0.4!important;}');
-
-        if (location.pathname.indexOf('/bai-hat/') === 0) {
-            var $btn = $('<a>', {
-                    'class': 'button-style-1 pull-left bv-download',
-                    href: '#download',
-                    'data-id': location.pathname.match(/\/(\w+)\.html/)[1],
-                    html: '<i class="zicon icon-dl"></i>'
-                }),
-                $txt = $('<span>', {
-                    text: 'Tải nhạc 320kbps'
-                });
-
-            $('#tabService').replaceWith($btn.append($txt));
-        } else {
-            $('.fn-dlsong').replaceWith(function () {
-                var songId = $(this).data('item').slice(5);
-
-                return '<a title="Tải nhạc 320kbps" class="bv-download bv-icon" href="#download" data-id="' + songId + '"></a>';
-            });
-        }
-
-        $('.bv-download').on('click', function (e) {
-            e.preventDefault();
-            var $this = $(this);
-
-            $this.removeClass('bv-waiting bv-complete bv-error').addClass('bv-waiting');
-            linksVip($(this).data('id'));
-        });
-
+    if (location.pathname.indexOf('/bai-hat/') === 0) {
+        $('#tabService').replaceWith('<a class="button-style-1 pull-left bv-download" href="' + linksVip(location.pathname.match(/\/(\w+)\.html/)[1]) + '"><i class="zicon icon-dl"></i><span>Tải nhạc 320kbps</span></a>');
     } else {
-
-        if (window.name.indexOf('mp3Zing320') !== 0) return;
-
-        waitForKeyElements('#option_result .fa-cloud-download', function () {
-            var $link = $('#option_result a:contains(320 Kbps)');
-
-            if ($link.length) {
-                $link = $link.attr('href');
-                $link = $link.split('/thankyou/')[1];
-                $link = atob($link);
-
-                window.location.assign($link);
-                window.setTimeout(function () {
-                    postMessage('complete');
-                }, 1000);
-            } else {
-                postMessage('error');
-            }
+        $('.fn-dlsong').replaceWith(function () {
+            return '<a title="Tải nhạc 320kbps" class="bv-download bv-icon" href="' + linksVip($(this).data('item').slice(5)) + '"></a>';
         });
-
     }
 
 })(jQuery, window, document);
