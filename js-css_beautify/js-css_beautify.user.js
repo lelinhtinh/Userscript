242a8c51450c31a90f6d2d0798565329c74538d8
baivong <Zzbaivong@gmail.com>
2018-05-19 22:56:51 +0700

    v3.1.0 load offline resources

--- js-css_beautify/js-css_beautify.user.js
+++ js-css_beautify/js-css_beautify.user.js
@@ -2,20 +2,25 @@
 // @name         Javascript-css beautify
 // @namespace    http://devs.forumvi.com
 // @description  Beautify and syntax highlighting for source code javascript, json, css.
-// @version      3.0.0
+// @version      3.1.0
 // @icon         http://i.imgur.com/kz8nqz1.png
 // @author       Zzbaivong
 // @oujs:author  baivong
 // @license      MIT; https://baivong.mit-license.org/license.txt
 // @match        http://*/*
 // @match        https://*/*
+// @resource     js_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.7.5/beautify.min.js
+// @resource     css_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.7.5/beautify-css.min.js
+// @resource     hljs https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js
+// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
 // @noframes
 // @supportURL   https://github.com/lelinhtinh/Userscript/issues
 // @run-at       document-idle
-// @grant        none
+// @grant        GM.getResourceUrl
+// @grant        GM_getResourceURL
 // ==/UserScript==
 
-/* global importScripts */
+/* eslint-env worker, es6 */
 (function () {
     'use strict';
 
@@ -50,9 +55,9 @@
                     beautify = 'js_beautify';
 
                 if (e.data.lang === 'javascript') {
-                    importScripts('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.7.5/beautify.min.js');
+                    importScripts(e.data.libs[0]);
                 } else {
-                    importScripts('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.7.5/beautify-css.min.js');
+                    importScripts(e.data.libs[1]);
                     beautify = 'css_beautify';
                 }
                 source = self.window[beautify](source);
@@ -62,7 +67,7 @@
                     source: source
                 });
 
-                importScripts('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js');
+                importScripts(e.data.libs[2]);
                 source = self.window.hljs.highlight(e.data.lang, source, true).value;
 
                 source = source.split('\n');
@@ -89,7 +94,7 @@
             pre = doc.createElement('pre');
 
         if (e.data.action === 'beautify') {
-            addstyle('*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{counter-reset:line-numbers;white-space:pre-wrap}code::before{counter-increment:line-numbers;content:counter(line-numbers);display:block;position:absolute;left:-4.5em;top:0;width:4em;text-align:right;color:#60686f;white-space:pre}code{display:block;position:relative;margin-left:4em;padding-left:.5em;min-height:1em;border-left:1px solid #32363b}pre{padding:.5em .5em .5em 5em;border-left:1px solid #1d1f21}pre.hljs{padding-left:.5em;border-left:0 none}code::after{content:".";visibility:hidden} .hljs-comment,.hljs-quote{color:#969896}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c66}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#de935f}.hljs-attribute{color:#f0c674}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#b5bd68}.hljs-title,.hljs-section{color:#81a2be}.hljs-keyword,.hljs-selector-tag{color:#b294bb}.hljs{display:block;overflow-x:auto;background:#1d1f21;color:#c5c8c6;padding:.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:700}');
+            addstyle('*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{counter-reset:line-numbers;white-space:pre-wrap;word-wrap:break-word;word-break:break-all}code::before{counter-increment:line-numbers;content:counter(line-numbers);display:block;position:absolute;left:-4.5em;top:0;width:4em;text-align:right;color:#60686f;white-space:pre}code{display:block;position:relative;margin-left:4em;padding-left:.5em;min-height:1em;border-left:1px solid #32363b}pre{padding:.5em .5em .5em 5em;border-left:1px solid #1d1f21}pre.hljs{padding-left:.5em;border-left:0 none}code::after{content:".";visibility:hidden} .hljs-comment,.hljs-quote{color:#969896}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c66}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#de935f}.hljs-attribute{color:#f0c674}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#b5bd68}.hljs-title,.hljs-section{color:#81a2be}.hljs-keyword,.hljs-selector-tag{color:#b294bb}.hljs{display:block;overflow-x:auto;background:#1d1f21;color:#c5c8c6;padding:.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:700}');
 
             pre.textContent = e.data.source;
         } else {
@@ -103,9 +108,16 @@
         if (e.data.action === 'beautify') output = doc.getElementsByTagName('pre')[0];
     };
 
-    worker.postMessage({
-        lang: lang,
-        content: output.textContent
+    var js_beautify = GM.getResourceUrl('js_beautify'),
+        css_beautify = GM.getResourceUrl('css_beautify'),
+        hljs = GM.getResourceUrl('hljs');
+
+    Promise.all([js_beautify, css_beautify, hljs]).then(function (urls) {
+        worker.postMessage({
+            libs: urls,
+            lang: lang,
+            content: output.textContent
+        });
     });
 
 }());
--- view_source/view_source.user.js
+++ view_source/view_source.user.js
@@ -2,18 +2,24 @@
 // @name         viewsource
 // @namespace    devs.forumvi.com
 // @description  View and beautify page source. Shortcut: Alt+U.
-// @version      3.0.0
+// @version      3.1.0
 // @icon         http://i.imgur.com/6yZMOeH.png
 // @author       Zzbaivong
 // @oujs:author  baivong
 // @license      MIT; https://baivong.mit-license.org/license.txt
 // @match        http://*/*
 // @match        https://*/*
+// @resource     js_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.7.5/beautify.min.js
+// @resource     css_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.7.5/beautify-css.min.js
+// @resource     html_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.7.5/beautify-html.min.js
+// @resource     hljs https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js
 // @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
 // @noframes
 // @connect      *
 // @supportURL   https://github.com/lelinhtinh/Userscript/issues
 // @run-at       document-idle
+// @grant        GM.getResourceUrl
+// @grant        GM_getResourceURL
 // @grant        GM.xmlHttpRequest
 // @grant        GM_xmlhttpRequest
 // @grant        GM.openInTab
@@ -21,7 +27,7 @@
 // @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-/* global importScripts */
+/* eslint-env worker, es6 */
 (function () {
     'use strict';
 
@@ -70,15 +76,17 @@
             self.onmessage = function (e) {
                 var source = e.data.content;
 
-                importScripts('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.7.5/beautify-html.min.js');
-                source = self.window.html_beautify(source);
+                importScripts(e.data.libs[0]);
+                importScripts(e.data.libs[1]);
+                importScripts(e.data.libs[2]);
+                source = self.window.html_beautify(source, { indent_scripts: 'keep' });
 
                 self.postMessage({
                     action: 'beautify',
                     source: source
                 });
 
-                importScripts('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js');
+                importScripts(e.data.libs[3]);
                 source = self.window.hljs.highlight('xml', source, true).value;
 
                 source = source.split('\n');
@@ -104,7 +112,7 @@
             pre = doc.createElement('pre');
 
         if (e.data.action === 'beautify') {
-            addstyle('*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{counter-reset:line-numbers;white-space:pre-wrap}code::before{counter-increment:line-numbers;content:counter(line-numbers);display:block;position:absolute;left:-4.5em;top:0;width:4em;text-align:right;color:#60686f;white-space:pre}code{display:block;position:relative;margin-left:4em;padding-left:.5em;min-height:1em;border-left:1px solid #32363b}pre{padding:.5em .5em .5em 5em;border-left:1px solid #1d1f21}pre.hljs{padding-left:.5em;border-left:0 none}code::after{content:".";visibility:hidden}a{color:#b5bd68}a:active,a:hover,a:visited{color:#8b9433} .hljs-comment,.hljs-quote{color:#969896}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c66}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#de935f}.hljs-attribute{color:#f0c674}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#b5bd68}.hljs-title,.hljs-section{color:#81a2be}.hljs-keyword,.hljs-selector-tag{color:#b294bb}.hljs{display:block;overflow-x:auto;background:#1d1f21;color:#c5c8c6;padding:.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:700}');
+            addstyle('*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{counter-reset:line-numbers;white-space:pre-wrap;word-wrap:break-word;word-break:break-all}code::before{counter-increment:line-numbers;content:counter(line-numbers);display:block;position:absolute;left:-4.5em;top:0;width:4em;text-align:right;color:#60686f;white-space:pre}code{display:block;position:relative;margin-left:4em;padding-left:.5em;min-height:1em;border-left:1px solid #32363b}pre{padding:.5em .5em .5em 5em;border-left:1px solid #1d1f21}pre.hljs{padding-left:.5em;border-left:0 none}code::after{content:".";visibility:hidden}a{color:#b5bd68}a:active,a:hover,a:visited{color:#8b9433} .hljs-comment,.hljs-quote{color:#969896}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c66}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#de935f}.hljs-attribute{color:#f0c674}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#b5bd68}.hljs-title,.hljs-section{color:#81a2be}.hljs-keyword,.hljs-selector-tag{color:#b294bb}.hljs{display:block;overflow-x:auto;background:#1d1f21;color:#c5c8c6;padding:.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:700}');
 
             pre.textContent = e.data.source;
             fragment.appendChild(pre);
@@ -134,13 +142,22 @@
         }
     };
 
+    var js_beautify = GM.getResourceUrl('js_beautify'),
+        css_beautify = GM.getResourceUrl('css_beautify'),
+        html_beautify = GM.getResourceUrl('html_beautify'),
+        hljs = GM.getResourceUrl('hljs');
+
     GM.xmlHttpRequest({
         method: 'GET',
         url: urlbeautify,
         onload: function (response) {
             doc.title = 'beautify-source:' + urlbeautify;
-            worker.postMessage({
-                content: response.response
+
+            Promise.all([js_beautify, css_beautify, html_beautify, hljs]).then(function (urls) {
+                worker.postMessage({
+                    libs: urls,
+                    content: response.response
+                });
             });
 
             var baseUrl,
