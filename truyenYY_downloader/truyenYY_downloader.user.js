c80650e6c0e78ef073b7fffac2f827a580e1e857
baivong <Zzbaivong@gmail.com>
2019-05-17 15:51:29 +0700

    Fix truyenYY cover

--- truyenYY_downloader/truyenYY_downloader.user.js
+++ truyenYY_downloader/truyenYY_downloader.user.js
@@ -2,7 +2,7 @@
 // @name         TruyenYY downloader
 // @namespace    http://devs.forumvi.com/
 // @description  Tải truyện từ truyenyy.com định dạng epub
-// @version      4.6.0
+// @version      4.6.1
 // @icon         https://i.imgur.com/1HkQv2b.png
 // @author       Zzbaivong
 // @oujs:author  baivong
@@ -11,7 +11,7 @@
 // @require      https://code.jquery.com/jquery-3.4.1.min.js
 // @require      https://unpkg.com/jszip@3.2.1/dist/jszip.min.js
 // @require      https://unpkg.com/ejs@2.6.1/ejs.min.js
-// @require      https://unpkg.com/jepub@2.1.0/dist/jepub.min.js
+// @require      https://unpkg.com/jepub@2.1.1/dist/jepub.min.js
 // @require      https://unpkg.com/file-saver@2.0.2/dist/FileSaver.min.js
 // @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
 // @noframes
@@ -162,7 +162,7 @@
 
         ebookTitle = $('h1.name').text().trim(),
         ebookAuthor = $('h2.author').text().trim(),
-        ebookCover = $('.novel-info .zoom-me').attr('src'),
+        ebookCover = $('.novel-info .cover img').data('src'),
         ebookDesc = $('#id_novel_summary').html(),
         ebookType = [],
         beginEnd = '',
