6bbe59c07cdca27cd120a9761747c634af8cfa52
baivong <Zzbaivong@gmail.com>
2019-06-03 10:20:06 +0700

    v1.10.7

--- manga_comic_downloader/manga_comic_downloader.user.js
+++ manga_comic_downloader/manga_comic_downloader.user.js
@@ -2,7 +2,7 @@
 // @name         manga comic downloader
 // @namespace    https://baivong.github.io
 // @description  Tải truyện tranh từ các trang chia sẻ ở Việt Nam. Nhấn Alt+Y để tải toàn bộ.
-// @version      1.10.6
+// @version      1.10.7
 // @icon         https://i.imgur.com/ICearPQ.png
 // @author       Zzbaivong
 // @license      MIT; https://baivong.mit-license.org/license.txt
