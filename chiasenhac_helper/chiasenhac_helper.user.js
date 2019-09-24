d6efde18a969bd89342d54d51bffaac26945975e
lelinhtinh <Zzbaivong@gmail.com>
2019-09-25 05:25:19 +0700

    Get real file size

--- chiasenhac_helper/chiasenhac_helper.user.js
+++ chiasenhac_helper/chiasenhac_helper.user.js
@@ -2,21 +2,40 @@
 // @name         ChiaSeNhac Helper
 // @namespace    https://lelinhtinh.github.io
 // @description  Download lossless music directly, bypass sharing dialog.
-// @version      1.1.0
+// @version      1.2.0
 // @icon         https://i.imgur.com/HLqWP3g.png
 // @author       lelinhtinh
 // @oujs:author  baivong
 // @license      MIT; https://baivong.mit-license.org/license.txt
 // @match        https://chiasenhac.vn/*
 // @match        https://*.chiasenhac.vn/*
+// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
 // @noframes
 // @supportURL   https://github.com/lelinhtinh/Userscript/issues
 // @run-at       document-idle
-// @grant        none
+// @grant        GM.xmlHttpRequest
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function () {
     'use strict';
+
+    const getFileSize = (url, callback) => {
+        GM.xmlHttpRequest({
+            url: url,
+            method: 'HEAD',
+            onload: response => {
+                let size = response.responseHeaders;
+
+                size = size.match(/^content\-length:\s(\d+)/im);
+                if (!size) callback(null);
+
+                size = parseInt(size[1], 10);
+                size = (size / 1048576).toFixed(2);
+                callback(size);
+            }
+        });
+    };
 
     const getFlacLink = downloadLink => downloadLink
         .replace(/\/(128|320|m4a|32)\//, '/flac/')
@@ -34,17 +53,22 @@
 
         const $parent = $down500kbps.parentNode;
         let tempSize = $down500kbps.textContent.trim().match(/([\d.]+)\sMB$/)[1];
+        const flacLink = getFlacLink($down500kbps.href);
 
         tempSize = Number(tempSize) * 2.4;
         tempSize = parseFloat(tempSize).toFixed(2);
 
         $parent.insertAdjacentHTML('afterEnd', `
             <li>
-                <a class="download_item" target="_blank" href="${getFlacLink($down500kbps.href)}" title="${$down500kbps.title}">
-                    <i class="material-icons">file_download</i> Link tải nhạc <span class="c4">FLAC Lossless</span> ${tempSize} MB
+                <a class="download_item" target="_blank" href="${flacLink}" title="${$down500kbps.title}">
+                    <i class="material-icons">file_download</i> Link tải nhạc <span class="c4">FLAC Lossless</span> <span class="fake_size">${tempSize}</span> MB
                 </a>
             </li>
         `);
+
+        getFileSize(flacLink, size => {
+            if (size) document.querySelector('.download_item .fake_size').textContent = size;
+        });
 
     }
 
