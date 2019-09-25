16265a9362a853f8ed7842f5659d37aace6d02d5
lelinhtinh <Zzbaivong@gmail.com>
2019-09-25 21:34:19 +0700

    Facebook Adblocker

---
+++ fb_adblock/fb_adblock.user.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name         Facebook Adblocker
+// @namespace    https://lelinhtinh.github.io
+// @description  Block all ads in Facebook News Feed
+// @version      1.0.0
+// @icon         https://i.imgur.com/F8ai0jB.png
+// @author       lelinhtinh
+// @oujs:author  baivong
+// @license      MIT; https://baivong.mit-license.org/license.txt
+// @match        https://www.facebook.com/*
+// @noframes
+// @supportURL   https://github.com/lelinhtinh/Userscript/issues
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    /**
+     * Logging level
+     * @type {Number}
+     */
+    const DEBUG = 0;
+
+
+    /* === DO NOT CHANGE === */
+    let countAds = 0;
+
+    const config = {
+        attributes: false,
+        childList: true,
+        subtree: true
+    };
+
+    const removeAds = wrap => {
+        if (DEBUG >= 2) console.log(wrap, 'wrapNode');
+
+        const subtiltes = wrap.querySelectorAll('[data-testid="testid-story-subtilte"] a');
+        if (!subtiltes.length) return;
+
+        Array.from(subtiltes).forEach(v => {
+            if (v.textContent.trim().search(/Được tài trợ|Bài viết được đề xuất|Sponsor|Suggest|Recommend/i) !== -1) {
+                v = v.closest('[data-testid="fbfeed_story"]');
+                if (DEBUG) console.log(++countAds, 'countAds');
+                if (DEBUG >= 3) console.log(v.innerHTML, 'htmlAds');
+                v.remove();
+            }
+        });
+    };
+
+    const init = () => {
+        if (DEBUG) console.log('Facebook Adblocker');
+        countAds = 0;
+
+        const newsFeed = document.querySelector('[data-testid="newsFeedStream"]');
+        if (DEBUG >= 2) console.log(newsFeed, 'newsFeedNode');
+        if (!newsFeed) return;
+
+        const observer = new MutationObserver(mutationsList => {
+            for (let mutation of mutationsList) {
+                removeAds(mutation.target);
+            }
+        });
+        observer.observe(newsFeed, config);
+
+        removeAds(document);
+    };
+
+    init();
+
+    const observer = new MutationObserver(init);
+    observer.observe(document.head, config);
+
+}());
