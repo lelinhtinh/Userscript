b8c74bcc6ef8cd0abfd6968e4cbb29143d6f2309
lelinhtinh <Zzbaivong@gmail.com>
2019-09-30 16:00:11 +0700

    Delete line numbers to improve performance

--- js-css_beautify/js-css_beautify.user.js
+++ js-css_beautify/js-css_beautify.user.js
@@ -2,27 +2,40 @@
 // @name         Javascript-css beautify
 // @namespace    http://devs.forumvi.com
 // @description  Beautify and syntax highlighting for source code javascript, json, css.
-// @version      3.1.1
+// @version      3.2.0
 // @icon         http://i.imgur.com/kz8nqz1.png
 // @author       Zzbaivong
 // @oujs:author  baivong
 // @license      MIT; https://baivong.mit-license.org/license.txt
 // @match        http://*/*
 // @match        https://*/*
-// @resource     js_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.9.0/beautify.min.js
-// @resource     css_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.9.0/beautify-css.min.js
-// @resource     hljs https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/highlight.min.js
+// @resource     js_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.2/beautify.min.js
+// @resource     css_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.2/beautify-css.min.js
+// @resource     hljs https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js
+// @resource     dark https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/atom-one-dark.min.css
+// @resource     light https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/atom-one-light.min.css
 // @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
 // @noframes
 // @supportURL   https://github.com/lelinhtinh/Userscript/issues
 // @run-at       document-idle
 // @grant        GM.getResourceUrl
 // @grant        GM_getResourceURL
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
 // ==/UserScript==
 
 /* eslint-env worker, es6 */
 (function () {
     'use strict';
+
+    /**
+     * Color themes
+     * @type {String} dark|light
+     */
+    const STYLE = 'dark';
+
+
+    /* === DO NOT CHANGE === */
 
     var doc = document,
         contenttype = doc.contentType,
@@ -32,17 +45,7 @@
 
     var output = doc.getElementsByTagName('pre')[0],
         lang = 'javascript',
-        blobURL, worker,
-
-        addstyle = function (aCss) {
-            var head = doc.getElementsByTagName('head')[0];
-            if (!head) return null;
-            var style = doc.createElement('style');
-            style.setAttribute('type', 'text/css');
-            style.textContent = aCss;
-            head.appendChild(style);
-            return style;
-        };
+        blobURL, worker;
 
     if (contenttype === 'text/css' || /.+\.css$/.test(pathname)) lang = 'css';
 
@@ -62,20 +65,10 @@
                 }
                 source = self.window[beautify](source);
 
-                self.postMessage({
-                    action: 'beautify',
-                    source: source
-                });
-
                 importScripts(e.data.libs[2]);
                 source = self.window.hljs.highlight(e.data.lang, source, true).value;
 
-                source = source.split('\n');
-                source = source.join('</code><code>');
-                source = '<code>' + source + '</code>';
-
                 self.postMessage({
-                    action: 'hljs',
                     source: source
                 });
             };
@@ -93,24 +86,19 @@
         var fragment = doc.createDocumentFragment(),
             pre = doc.createElement('pre');
 
-        if (e.data.action === 'beautify') {
-            addstyle('*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{counter-reset:line-numbers;white-space:pre-wrap;word-wrap:break-word;word-break:break-all}code::before{counter-increment:line-numbers;content:counter(line-numbers);display:block;position:absolute;left:-4.5em;top:0;width:4em;text-align:right;color:#60686f;white-space:pre}code{display:block;position:relative;margin-left:4em;padding-left:.5em;min-height:1em;border-left:1px solid #32363b}pre{padding:.5em .5em .5em 5em;border-left:1px solid #1d1f21}pre.hljs{padding-left:.5em;border-left:0 none}code::after{content:".";visibility:hidden} .hljs-comment,.hljs-quote{color:#969896}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c66}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#de935f}.hljs-attribute{color:#f0c674}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#b5bd68}.hljs-title,.hljs-section{color:#81a2be}.hljs-keyword,.hljs-selector-tag{color:#b294bb}.hljs{display:block;overflow-x:auto;background:#1d1f21;color:#c5c8c6;padding:.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:700}');
-
-            pre.textContent = e.data.source;
-        } else {
-            pre.innerHTML = e.data.source;
-            pre.className = 'hljs ' + lang;
-        }
+        pre.innerHTML = e.data.source;
+        pre.className = 'hljs ' + lang;
 
         fragment.appendChild(pre);
         doc.body.replaceChild(fragment, output);
-
-        if (e.data.action === 'beautify') output = doc.getElementsByTagName('pre')[0];
     };
 
     var js_beautify = GM.getResourceUrl('js_beautify'),
         css_beautify = GM.getResourceUrl('css_beautify'),
-        hljs = GM.getResourceUrl('hljs');
+        hljs = GM.getResourceUrl('hljs'),
+        style = GM_getResourceText(STYLE);
+
+    GM_addStyle('*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{white-space:pre-wrap;word-wrap:break-word;word-break:break-all}' + style);
 
     Promise.all([js_beautify, css_beautify, hljs]).then(function (urls) {
         worker.postMessage({
--- view_source/view_source.user.js
+++ view_source/view_source.user.js
@@ -2,17 +2,19 @@
 // @name         viewsource
 // @namespace    devs.forumvi.com
 // @description  View and beautify page source. Shortcut: Alt+U.
-// @version      3.1.1
+// @version      3.2.0
 // @icon         http://i.imgur.com/6yZMOeH.png
 // @author       Zzbaivong
 // @oujs:author  baivong
 // @license      MIT; https://baivong.mit-license.org/license.txt
 // @match        http://*/*
 // @match        https://*/*
-// @resource     js_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.9.0/beautify.min.js
-// @resource     css_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.9.0/beautify-css.min.js
-// @resource     html_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.9.0/beautify-html.min.js
-// @resource     hljs https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/highlight.min.js
+// @resource     js_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.2/beautify.min.js
+// @resource     css_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.2/beautify-css.min.js
+// @resource     html_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.2/beautify-html.min.js
+// @resource     hljs https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js
+// @resource     dark https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/atom-one-dark.min.css
+// @resource     light https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/atom-one-light.min.css
 // @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
 // @noframes
 // @connect      *
@@ -20,6 +22,8 @@
 // @run-at       document-idle
 // @grant        GM.getResourceUrl
 // @grant        GM_getResourceURL
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
 // @grant        GM.xmlHttpRequest
 // @grant        GM_xmlhttpRequest
 // @grant        GM.openInTab
@@ -30,6 +34,15 @@
 /* eslint-env worker, es6 */
 (function () {
     'use strict';
+
+    /**
+     * Color themes
+     * @type {String} dark|light
+     */
+    const STYLE = 'dark';
+
+
+    /* === DO NOT CHANGE === */
 
     var doc = document,
         urlpage = location.href,
@@ -57,17 +70,7 @@
     urlbeautify = urlpage.replace(urlbeautify, '');
     urlbeautify = decodeURIComponent(urlbeautify);
 
-    var blobURL, worker,
-
-        addstyle = function (aCss) {
-            var head = doc.getElementsByTagName('head')[0];
-            if (!head) return null;
-            var style = doc.createElement('style');
-            style.setAttribute('type', 'text/css');
-            style.textContent = aCss;
-            head.appendChild(style);
-            return style;
-        };
+    var blobURL, worker;
 
     blobURL = URL.createObjectURL(new Blob(['(',
         function () {
@@ -79,22 +82,14 @@
                 importScripts(e.data.libs[0]);
                 importScripts(e.data.libs[1]);
                 importScripts(e.data.libs[2]);
-                source = self.window.html_beautify(source, { indent_scripts: 'keep' });
-
-                self.postMessage({
-                    action: 'beautify',
-                    source: source
+                source = self.window.html_beautify(source, {
+                    indent_scripts: 'keep'
                 });
 
                 importScripts(e.data.libs[3]);
                 source = self.window.hljs.highlight('xml', source, true).value;
 
-                source = source.split('\n');
-                source = source.join('</code><code>');
-                source = '<code>' + source + '</code>';
-
                 self.postMessage({
-                    action: 'hljs',
                     source: source
                 });
             };
@@ -108,36 +103,30 @@
 
     worker.onmessage = function (e) {
         if (!e.data) return;
+
         var fragment = doc.createDocumentFragment(),
             pre = doc.createElement('pre');
 
-        if (e.data.action === 'beautify') {
-            addstyle('*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{counter-reset:line-numbers;white-space:pre-wrap;word-wrap:break-word;word-break:break-all}code::before{counter-increment:line-numbers;content:counter(line-numbers);display:block;position:absolute;left:-4.5em;top:0;width:4em;text-align:right;color:#60686f;white-space:pre}code{display:block;position:relative;margin-left:4em;padding-left:.5em;min-height:1em;border-left:1px solid #32363b}pre{padding:.5em .5em .5em 5em;border-left:1px solid #1d1f21}pre.hljs{padding-left:.5em;border-left:0 none}code::after{content:".";visibility:hidden}a{color:#b5bd68}a:active,a:hover,a:visited{color:#8b9433} .hljs-comment,.hljs-quote{color:#969896}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c66}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#de935f}.hljs-attribute{color:#f0c674}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#b5bd68}.hljs-title,.hljs-section{color:#81a2be}.hljs-keyword,.hljs-selector-tag{color:#b294bb}.hljs{display:block;overflow-x:auto;background:#1d1f21;color:#c5c8c6;padding:.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:700}');
+        pre.innerHTML = e.data.source;
+        pre.className = 'hljs xml';
 
-            pre.textContent = e.data.source;
-            fragment.appendChild(pre);
-            doc.body.appendChild(fragment);
-        } else {
-            pre.innerHTML = e.data.source;
-            pre.className = 'hljs xml';
-            fragment.appendChild(pre);
-            doc.body.replaceChild(fragment, doc.getElementsByTagName('pre')[0]);
+        fragment.appendChild(pre);
+        doc.body.appendChild(fragment);
 
-            var attrUrl = doc.getElementsByClassName('hljs-attr');
-            for (var j = 0; j < attrUrl.length; j++) {
-                if (/\b(src|href\b)/.test(attrUrl[j].textContent)) {
-                    var link = attrUrl[j].nextSibling.nextSibling,
-                        url = link.textContent,
-                        quote = url.slice(0, 1);
+        var attrUrl = doc.getElementsByClassName('hljs-attr');
+        for (var j = 0; j < attrUrl.length; j++) {
+            if (/\b(src|href\b)/.test(attrUrl[j].textContent)) {
+                var link = attrUrl[j].nextSibling.nextSibling,
+                    url = link.textContent,
+                    quote = url.slice(0, 1);
 
-                    if (quote !== '\'' && quote !== '"') {
-                        quote = '';
-                    } else {
-                        url = url.slice(1, -1);
-                    }
-
-                    link.innerHTML = quote + '<a href="' + url + '" target="_blank">' + url + '</a>' + quote;
+                if (quote !== '\'' && quote !== '"') {
+                    quote = '';
+                } else {
+                    url = url.slice(1, -1);
                 }
+
+                link.innerHTML = quote + '<a href="' + url + '" target="_blank">' + url + '</a>' + quote;
             }
         }
     };
@@ -145,7 +134,10 @@
     var js_beautify = GM.getResourceUrl('js_beautify'),
         css_beautify = GM.getResourceUrl('css_beautify'),
         html_beautify = GM.getResourceUrl('html_beautify'),
-        hljs = GM.getResourceUrl('hljs');
+        hljs = GM.getResourceUrl('hljs'),
+        style = GM_getResourceText(STYLE);
+
+    GM_addStyle('*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{white-space:pre-wrap;word-wrap:break-word;word-break:break-all}' + style);
 
     GM.xmlHttpRequest({
         method: 'GET',
