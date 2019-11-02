8e240621a7558da397e019ad8bf91df573c991d7
lelinhtinh <Zzbaivong@gmail.com>
2019-11-02 19:47:02 +0700

    Complete function of Watch VIP video online

--- phim400_helper/phim4400_helper.user.js
+++ phim400_helper/phim4400_helper.user.js
@@ -4,7 +4,7 @@
 // @namespace       https://lelinhtinh.github.io
 // @description     Xem online và tải phim trực tiếp tại Phim4400, bỏ qua quảng cáo.
 // @description:vi  Xem online và tải phim trực tiếp tại Phim4400, bỏ qua quảng cáo.
-// @version         1.1.0
+// @version         1.2.0
 // @icon            https://i.imgur.com/wRRkkqr.png
 // @author          lelinhtinh
 // @oujs:author     baivong
@@ -25,105 +25,6 @@
 (function() {
   'use strict';
 
-  const upfileVn = document.querySelector('a[href*="upfile.vn"]');
-  const upfileUs = document.querySelector('a[href*="upfile.us"]');
-  const drivehub = document.querySelector('a[href*="drivehub.link"]');
-
-  let linkDown = {
-    upfileVn: {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      getLink: response => {
-        return JSON.parse(response).Link;
-      },
-    },
-    drivehub: {
-      method: 'GET',
-      getLink: response => {
-        let parser = new DOMParser();
-        let html = parser.parseFromString(response, 'text/html');
-        return html.getElementById('proceed').href;
-      },
-    },
-    upfileUs: {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'X-Requested-With': 'XMLHttpRequest',
-      },
-      data: 'type=file_check',
-      getLink: response => {
-        console.log(response);
-        return JSON.parse(response).debug[0].file_source;
-      },
-    },
-  };
-
-  if (upfileVn !== null) {
-    linkDown.upfileVn.url = upfileVn.href;
-    linkDown.upfileVn.data = 'Token=' + sha256(upfileVn.href.split('://')[1].split('/')[1] + '7891').toUpperCase();
-  }
-  if (upfileUs !== null) linkDown.upfileUs.url = upfileUs.href;
-  if (drivehub !== null) linkDown.drivehub.url = drivehub.href;
-
-  let $videoFrame = document.createElement('video');
-  $videoFrame.controls = true;
-  $videoFrame.setAttribute('preload', 'auto');
-  $videoFrame.setAttribute('width', '100%');
-  const getVideo = type => {
-    GM.xmlHttpRequest({
-      url: linkDown[type].url,
-      method: linkDown[type].method || 'POST',
-      headers: linkDown[type].headers,
-      data: linkDown[type].data,
-      onload: response => {
-        linkDown[type].link = linkDown[type].getLink(response.response);
-        if ($videoFrame.canPlayType('video/mp4')) $videoFrame.setAttribute('src', linkDown[type].link);
-        console.log(linkDown[type].link);
-        if (document.getElementById('video-frame') !== null) {
-          document.getElementById('video-frame').appendChild($videoFrame);
-        }
-      },
-    });
-  };
-  let listServer = [];
-  Object.keys(linkDown).forEach((btnSwitchServer, index) => {
-    let serverLi = document.createElement('LI');
-    let serverBtn = document.createElement('A');
-
-    serverBtn.textContent = `SERVER ${index + 1}`;
-    serverBtn.className = 'button-server';
-    serverBtn.setAttribute('data-type', btnSwitchServer);
-    serverLi.appendChild(serverBtn);
-    listServer.push(serverLi);
-  });
-
-  let $listViewBtn = document.querySelector('.uk-accordion');
-
-  if ($listViewBtn !== null) {
-    let $videoTemplate = `<ul class="uk-subnav uk-subnav-pill" uk-switcher></ul>
-      <ul class="uk-switcher uk-margin">
-        <li id="video-frame"></li>
-      </ul>
-    `;
-    let $videoFrame = document.querySelector('.xem-phim.uk-panel');
-    if (document.querySelector('.xem-phim.uk-panel > ul') === null) {
-      $videoFrame.insertAdjacentHTML('beforeend', $videoTemplate);
-      let navSelectServer = document.querySelector('.uk-subnav.uk-subnav-pill');
-      listServer.forEach(sv => {
-        navSelectServer.appendChild(sv);
-      });
-    }
-    getVideo('upfileVn');
-    $listViewBtn.addEventListener('click', e => {
-      e.preventDefault();
-      if (e.target.classList.contains('button-server')) {
-        getVideo(e.target.dataset.type);
-      }
-    });
-  }
-
-  //
   document.oncontextmenu = null;
   if (shortcut) {
     shortcut.remove('Ctrl+U');
@@ -133,21 +34,180 @@
     shortcut.remove('Ctrl+Shift+C');
   }
 
-  let $btn = document.querySelector('.button-phim'),
-    link = document.querySelector('.fb-comments');
+  if (location.pathname.indexOf('/phim/') === -1) {
+    let $btn = document.querySelector('.button-phim'),
+      link = document.querySelector('.fb-comments');
 
-  if (link === null) return;
-  link = link.dataset.href;
-  if (link.indexOf('/phim/') === -1) return;
+    if (link === null) return;
+    link = link.dataset.href;
+    if (link.indexOf('/phim/') === -1) return;
 
-  if ($btn === null) {
-    $btn = document.createElement('A');
-    $btn.textContent = 'Tải Phim VIP';
-    document.querySelector('.button-info').appendChild($btn);
+    if ($btn === null) {
+      $btn = document.createElement('a');
+      $btn.textContent = 'Xem Online và Tải Phim';
+      document.querySelector('.button-info').appendChild($btn);
+    }
+
+    $btn.className = 'button-phim uk-button uk-button-secondary uk-dropdown-right';
+    $btn.setAttribute('target', '_top');
+
+    $btn.href = link;
+    return;
   }
 
-  $btn.className = 'button-phim uk-button uk-button-secondary uk-dropdown-right';
-  $btn.setAttribute('target', '_top');
+  const $videoWrap = document.querySelector('.xem-phim');
+  if ($videoWrap.innerHTML.trim() !== '') return;
 
-  $btn.href = link;
+  function renderVideo(src) {
+    let $video = document.querySelector('#vipPlayer');
+
+    if ($video === null) {
+      $video = document.createElement('video');
+      $video.id = 'vipPlayer';
+      $video.controls = true;
+      $video.preload = 'auto';
+      $video.setAttribute(
+        'style',
+        `
+          height: 450px;
+          width: 100%;
+        `
+      );
+      $video.addEventListener('error', () => {
+        $content.innerHTML = `
+          <p class="uk-text-center uk-card-body">
+            <em>
+              <big>Không thể phát video lúc này!</big>
+              <br>
+              <span>Hãy thử chuyển sang server khác.</span>
+            </em>
+          </p>
+          `;
+      });
+
+      $content.innerHTML = '';
+      $content.appendChild($video);
+    }
+
+    $video.src = src;
+    $video.play();
+  }
+
+  function getVideo(host) {
+    if (hostConfigs[host].src) {
+      const $aActive = $tabs.querySelector('li.uk-active a');
+      if ($aActive !== null && $aActive.dataset.host === host) return;
+
+      renderVideo(hostConfigs[host].src);
+      return;
+    }
+
+    if (host === 'upfileVn') {
+      hostConfigs.upfileVn.data =
+        'Token=' + sha256(hostConfigs.upfileVn.href.split('://')[1].split('/')[1] + '7891').toUpperCase();
+    }
+
+    GM.xmlHttpRequest({
+      url: hostConfigs[host].href,
+      method: hostConfigs[host].method || 'POST',
+      headers: hostConfigs[host].headers,
+      data: hostConfigs[host].data,
+
+      onload: response => {
+        let src = hostConfigs[host].getSrc(response.response);
+        hostConfigs[host].src = src;
+        renderVideo(src);
+      },
+    });
+  }
+
+  function playVideo(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const $this = e.target,
+      $active = $tabs.querySelector('li.uk-active');
+
+    getVideo($this.dataset.host);
+
+    if ($active !== null) $active.classList.remove('uk-active');
+    $this.parentNode.classList.add('uk-active');
+  }
+
+  const hostConfigs = {
+    upfileVn: {
+      selector: 'a[href*="upfile.vn"]',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      getSrc: response => {
+        return JSON.parse(response).Link;
+      },
+    },
+    drivehub: {
+      selector: 'a[href*="drivehub.link"]',
+      method: 'GET',
+      getSrc: response => {
+        const parser = new DOMParser();
+        const $html = parser.parseFromString(response, 'text/html');
+        const $proceed = $html.querySelector('#proceed');
+
+        return $proceed === null ? null : $proceed.href.replace(/\?e=download$/, '');
+      },
+    },
+    upfileUs: {
+      selector: 'a[href*="upfile.us"]',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      data: 'type=file_check',
+      getSrc: response => {
+        return JSON.parse(response).debug[0].file_source;
+      },
+    },
+  };
+
+  const $tabs = document.createElement('ul');
+  $tabs.className = 'uk-subnav uk-subnav-pill';
+
+  let hostCount = 0;
+  Object.keys(hostConfigs).forEach(host => {
+    const currHost = hostConfigs[host];
+    const $btn = document.querySelector('.uk-accordion-content ' + currHost.selector);
+    if ($btn === null) return;
+    hostConfigs[host].href = $btn.href;
+
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    a.textContent = 'Server ' + ++hostCount;
+    a.href = '#' + host;
+    a.setAttribute('data-host', host);
+    a.addEventListener('click', playVideo);
+
+    li.appendChild(a);
+    $tabs.appendChild(li);
+  });
+
+  const $contents = document.createElement('ul');
+  $contents.className = 'uk-switcher uk-margin';
+
+  const $content = document.createElement('li');
+  $content.className = 'uk-active';
+  $content.setAttribute(
+    'style',
+    `
+      background: #000;
+      border: 1px solid #6fb429;
+      border-top: 0px solid #0000;
+      box-sizing: border-box;
+    `
+  );
+  $contents.appendChild($content);
+
+  $videoWrap.appendChild($tabs);
+  $videoWrap.appendChild($contents);
+
+  $tabs.querySelector('a').click();
 })();
