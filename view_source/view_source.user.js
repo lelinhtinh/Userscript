dce7da34cd148ebe925633349b7add97a0a9347b
lelinhtinh <lelinhtinh2013@gmail.com>
2020-08-16 18:44:27 +0700

    Show view-source page by Blob URL

--- view_source/view_source.user.js
+++ view_source/view_source.user.js
@@ -4,19 +4,19 @@
 // @namespace       devs.forumvi.com
 // @description     View and beautify page source. Shortcut: Alt+U.
 // @description:vi  Định dạng và làm đẹp mã nguồn trang web. Phím tắt: Alt+U.
-// @version         3.2.4
+// @version         3.3.0
 // @icon            http://i.imgur.com/6yZMOeH.png
 // @author          Zzbaivong
 // @oujs:author     baivong
 // @license         MIT; https://baivong.mit-license.org/license.txt
 // @match           http://*/*
 // @match           https://*/*
-// @resource        js_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.2/beautify.min.js
-// @resource        css_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.2/beautify-css.min.js
-// @resource        html_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.2/beautify-html.min.js
-// @resource        hljs https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js
-// @resource        dark https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/atom-one-dark.min.css
-// @resource        light https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/atom-one-light.min.css
+// @resource        js_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.12.0/beautify.min.js
+// @resource        css_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.12.0/beautify-css.min.js
+// @resource        html_beautify https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.12.0/beautify-html.min.js
+// @resource        hljs https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js
+// @resource        dark https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/atom-one-dark.min.css
+// @resource        light https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/atom-one-light.min.css
 // @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
 // @noframes
 // @connect         *
@@ -33,7 +33,7 @@
 // ==/UserScript==
 
 /* eslint-env worker, es6 */
-(function() {
+(() => {
   'use strict';
 
   /**
@@ -44,43 +44,27 @@
 
   /* === DO NOT CHANGE === */
 
-  var doc = document,
-    urlpage = location.href,
-    urlbeautify = 'https://lelinhtinh.github.io/Userscript/?beautify-source=';
+  const urlpage = location.href;
 
-  if (!/^application\/(xhtml+xml|xml|rss+xml)|text\/(html|xml)$/.test(doc.contentType)) return;
+  if (!/^application\/(xhtml+xml|xml|rss+xml)|text\/(html|xml)$/.test(document.contentType)) return;
 
-  if (urlpage.indexOf(urlbeautify) !== 0) {
-    var viewsource = function() {
-      if (urlpage.indexOf(urlbeautify) === 0) return;
-      GM.openInTab(urlbeautify + encodeURIComponent(urlpage), false);
-    };
+  GM_registerMenuCommand('Beautify Page Source', viewsource, 'u');
+  document.onkeydown = (e) => {
+    if (e.which === 85 && e.altKey) {
+      // Alt+U
+      e.preventDefault();
+      viewsource();
+    }
+  };
 
-    GM_registerMenuCommand('Beautify Page Source', viewsource, 'u');
-    doc.onkeydown = function(e) {
-      if (e.which === 85 && e.altKey) {
-        // Alt+U
-        e.preventDefault();
-        viewsource();
-      }
-    };
-
-    return;
-  }
-
-  urlbeautify = urlpage.replace(urlbeautify, '');
-  urlbeautify = decodeURIComponent(urlbeautify);
-
-  var blobURL, worker;
-
-  blobURL = URL.createObjectURL(
+  const handleURL = URL.createObjectURL(
     new Blob(
       [
         '(',
-        function() {
+        function () {
           self.window = {};
 
-          self.onmessage = function(e) {
+          self.onmessage = (e) => {
             var source = e.data.content;
 
             importScripts(e.data.libs[0]);
@@ -91,9 +75,11 @@
             });
 
             importScripts(e.data.libs[3]);
-            source = self.window.hljs.highlight('xml', source, true).value;
+            source = self.hljs.highlight('xml', source, true).value;
 
             self.postMessage({
+              base: e.data.base,
+              theme: e.data.libs[4],
               source: source,
             });
           };
@@ -105,78 +91,82 @@
       }
     )
   );
-  worker = new Worker(blobURL);
 
-  worker.onmessage = function(e) {
+  const worker = new Worker(handleURL);
+  worker.onmessage = (e) => {
     if (!e.data) return;
 
-    var fragment = doc.createDocumentFragment(),
-      pre = doc.createElement('pre');
+    const viewSourceURL = URL.createObjectURL(
+      new Blob(
+        [
+          `<!DOCTYPE html>
+          <html>
+          <head>
+            <meta charset="utf-8">
+            <base href="${e.data.base}" target="_blank">
+            <title>beautify-source:${urlpage}</title>
+            <style>${e.data.theme}*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{white-space:pre-wrap;word-wrap:break-word;word-break:break-all}a{color:#4d4bd8}</style>
+          </head>
+          <body>
+            <pre class="hljs xml">${e.data.source}</pre>
+            <script>
+            const attrUrl = document.getElementsByClassName('hljs-attr');
+            for (let j = 0; j < attrUrl.length; j++) {
+              if (/\\b(src|href\\b)/.test(attrUrl[j].textContent)) {
+                let link = attrUrl[j].nextSibling.nextSibling,
+                  url = link.textContent,
+                  quote = url.slice(0, 1);
 
-    pre.innerHTML = e.data.source;
-    pre.className = 'hljs xml';
+                if (quote !== "'" && quote !== '"') {
+                  quote = '';
+                } else {
+                  url = url.slice(1, -1);
+                }
 
-    fragment.appendChild(pre);
-    doc.body.appendChild(fragment);
-
-    var attrUrl = doc.getElementsByClassName('hljs-attr');
-    for (var j = 0; j < attrUrl.length; j++) {
-      if (/\b(src|href\b)/.test(attrUrl[j].textContent)) {
-        var link = attrUrl[j].nextSibling.nextSibling,
-          url = link.textContent,
-          quote = url.slice(0, 1);
-
-        if (quote !== "'" && quote !== '"') {
-          quote = '';
-        } else {
-          url = url.slice(1, -1);
+                link.innerHTML = quote + '<a href="' + url + '" target="_blank">' + url + '</a>' + quote;
+              }
+            }
+            </script>
+          </body>
+          </html>`,
+        ],
+        {
+          type: 'text/html; charset=utf-8',
         }
+      )
+    );
 
-        link.innerHTML = quote + '<a href="' + url + '" target="_blank">' + url + '</a>' + quote;
-      }
-    }
+    GM.openInTab(urlpage, true);
+    location.href = viewSourceURL;
   };
 
-  var js_beautify = GM.getResourceUrl('js_beautify'),
-    css_beautify = GM.getResourceUrl('css_beautify'),
-    html_beautify = GM.getResourceUrl('html_beautify'),
-    hljs = GM.getResourceUrl('hljs');
-
-  GM.getResourceUrl(STYLE)
-    .then(function(url) {
-      return fetch(url);
-    })
-    .then(function(resp) {
-      return resp.text();
-    })
-    .then(function(style) {
-      GM_addStyle(
-        '*{margin:0;padding:0}html{line-height:1em;background:#1d1f21;color:#c5c8c6}pre{white-space:pre-wrap;word-wrap:break-word;word-break:break-all}' +
-          style + 'a{color:#29B6F6}a:hover,a:focus{color:#1E88E5}a:visited,a:active{color:#1565C0}'
-      );
-    });
-
-  GM.xmlHttpRequest({
-    method: 'GET',
-    url: urlbeautify,
-    onload: function(response) {
-      doc.title = 'beautify-source:' + urlbeautify;
-
-      Promise.all([js_beautify, css_beautify, html_beautify, hljs]).then(function(urls) {
-        worker.postMessage({
-          libs: urls,
-          content: response.response,
+  function viewsource() {
+    const js_beautify = GM.getResourceUrl('js_beautify'),
+      css_beautify = GM.getResourceUrl('css_beautify'),
+      html_beautify = GM.getResourceUrl('html_beautify'),
+      hljs = GM.getResourceUrl('hljs'),
+      theme = GM.getResourceUrl(STYLE)
+        .then(function (url) {
+          return fetch(url);
+        })
+        .then(function (resp) {
+          return resp.text();
         });
-      });
 
-      var baseUrl,
-        baseMatch = response.response.match(/<base\s+href="([^"]+)"\s?[^>]*>/),
-        base = doc.createElement('base');
+    GM.xmlHttpRequest({
+      method: 'GET',
+      url: urlpage,
+      onload: (response) => {
+        const baseMatch = response.response.match(/<base[\s\n]+href[\s\n]*=[\s\n]*("|')?([^"'\s\n]+)("|')?.*?\/?>/i);
 
-      baseUrl = baseMatch ? baseMatch[1] : urlbeautify.replace(/[^/]*$/, '');
-
-      base.href = baseUrl;
-      doc.head.appendChild(base);
-    },
-  });
+        Promise.all([js_beautify, css_beautify, html_beautify, hljs, theme]).then((urls) => {
+          worker.postMessage({
+            libs: urls,
+            base: baseMatch ? baseMatch[2] : urlpage.replace(/[^/]*$/, ''),
+            content: response.response,
+          });
+        });
+      },
+    });
+  }
 })();
