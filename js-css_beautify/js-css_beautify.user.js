2858e163eadd3dd2594acf25bf76926138781226
lelinhtinh <lelinhtinh2013@gmail.com>
2020-08-18 02:37:31 +0700

    Support for more formats

--- js-css_beautify/js-css_beautify.user.js
+++ js-css_beautify/js-css_beautify.user.js
@@ -2,19 +2,26 @@
 // @name            Javascript-css beautify
 // @name:vi         Javascript-css beautify
 // @namespace       http://devs.forumvi.com
-// @description     Beautify and syntax highlighting for source code JavaScript, JSON, CSS.
-// @description:vi  Định dạng và làm đẹp mã nguồn JavaScript, JSON, CSS.
-// @version         4.0.0
+// @description     Beautify and syntax highlighting for source code JavaScript, JSON, CSS. From v4.1+, a few more formats are also supported.
+// @description:vi  Định dạng và làm đẹp mã nguồn JavaScript, JSON, CSS. Từ bản v4.1+, một vài định dạng khác cũng được hỗ trợ .
+// @version         4.1.0
 // @icon            http://i.imgur.com/kz8nqz1.png
 // @author          Zzbaivong
 // @oujs:author     baivong
 // @license         MIT; https://baivong.mit-license.org/license.txt
 // @match           http://*/*
 // @match           https://*/*
-// @resource        prettier https://unpkg.com/prettier@2.0.5/standalone.js
-// @resource        parser-babel https://unpkg.com/prettier@2.0.5/parser-babel.js
-// @resource        parser-postcss https://unpkg.com/prettier@2.0.5/parser-postcss.js
-// @resource        hljs https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js
+// @require         https://unpkg.com/prettier@2.0.5/standalone.js
+// @require         https://unpkg.com/prettier@2.0.5/parser-postcss.js
+// @require         https://unpkg.com/prettier@2.0.5/parser-html.js
+// @require         https://unpkg.com/prettier@2.0.5/parser-babel.js
+// @require         https://unpkg.com/prettier@2.0.5/parser-graphql.js
+// @require         https://unpkg.com/prettier@2.0.5/parser-markdown.js
+// @require         https://unpkg.com/prettier@2.0.5/parser-typescript.js
+// @require         https://unpkg.com/prettier@2.0.5/parser-yaml.js
+// @require         https://unpkg.com/prettier@2.0.5/parser-angular.js
+// @require         https://unpkg.com/@prettier/plugin-php@0.14.3/standalone.js
+// @require         https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js
 // @resource        dark https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/atom-one-dark.min.css
 // @resource        light https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/atom-one-light.min.css
 // @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
@@ -24,9 +31,10 @@
 // @grant           GM.getResourceUrl
 // @grant           GM_getResourceURL
 // @grant           GM_addStyle
-// @grant           GM_getResourceText
+// @inject-into     content
 // ==/UserScript==
 
+/* global prettier, prettierPlugins, hljs */
 /* eslint-env worker, es6 */
 (() => {
   'use strict';
@@ -39,93 +47,71 @@
 
   /* === DO NOT CHANGE === */
 
-  const doc = document,
-    contentType = doc.contentType,
+  const output = document.querySelector('body > pre');
+  if (output === null) return;
+  if (document.querySelector('body').firstElementChild.tagName !== 'PRE') return;
+
+  const contentType = document.contentType,
     pathname = location.pathname;
 
-  if (
-    !(
-      /^application\/(x-javascript|javascript|json)|text\/css$/.test(contentType) ||
-      (/.+\.(js|json|css)$/.test(pathname) &&
-        !/^application\/(xhtml+xml|xml|rss+xml)|text\/(html|xml)$/.test(contentType))
-    )
-  )
-    return;
+  if (/^application\/(xhtml+xml|xml|rss+xml)|text\/(html|xml)$/.test(contentType)) return;
 
   let parser;
   if (contentType === 'text/css' || /.+\.css$/.test(pathname)) {
     parser = 'css';
-  } else if (contentType === 'application/json' || /.+\.json$/.test(pathname)) {
+  } else if (contentType === 'application/json' || /.+\.(json|map)$/.test(pathname)) {
     parser = 'json';
-  } else {
+  } else if (/^application\/(x-javascript|javascript)$/.test(contentType) || /.+\.jsx?$/.test(pathname)) {
     parser = 'babel';
+  } else if (contentType === 'text/plain') {
+    if (/.+\.component\.html$/.test(pathname)) {
+      parser = 'angular';
+    } else if (/.+\.(gql|graphql)$/.test(pathname)) {
+      parser = 'graphql';
+    } else if (/.+\.ya?ml$/.test(pathname)) {
+      parser = 'yaml';
+    } else if (/.+\.(x?html?|xml)$/.test(pathname)) {
+      parser = 'html';
+    } else if (/.+\.tsx?$/.test(pathname)) {
+      parser = 'typescript';
+    } else if (/.+\.php$/.test(pathname)) {
+      parser = 'php';
+    } else if (/.+\.vue$/.test(pathname)) {
+      parser = 'vue';
+    } else if (/.+\.(less|scss)$/.test(pathname)) {
+      parser = 'css';
+    } else if (/.+\.(md|markdown)$/.test(pathname)) {
+      parser = 'markdown';
+    }
   }
 
-  const blobURL = URL.createObjectURL(
-    new Blob(
-      [
-        '(',
-        function () {
-          self.onmessage = (e) => {
-            let source = e.data.content;
-
-            importScripts(e.data.libs[0]);
-            importScripts(e.data.libs[1]);
-            /* global prettierPlugins */
-            source = prettier.format(source, { parser: e.data.parser, plugins: prettierPlugins });
-
-            importScripts(e.data.libs[2]);
-            source = hljs.highlight(e.data.parser === 'babel' ? 'javascript' : e.data.parser, source, true).value;
-
-            self.postMessage({
-              theme: e.data.libs[3],
-              source: source,
-            });
-          };
-        }.toString(),
-        ')()',
-      ],
-      {
-        type: 'application/javascript',
-      }
-    )
-  );
-
-  const output = doc.getElementsByTagName('pre')[0];
-  const worker = new Worker(blobURL);
-
-  worker.onmessage = (e) => {
-    if (!e.data) return;
-
-    const fragment = doc.createDocumentFragment(),
-      pre = doc.createElement('pre');
-
-    pre.innerHTML = e.data.source;
-    pre.className = 'hljs';
-
-    fragment.appendChild(pre);
-    doc.body.replaceChild(fragment, output);
-
-    GM_addStyle(
-      `${e.data.theme}*{margin:0;padding:0}html{line-height:1em;background:${
-        STYLE === 'dark' ? '#282c34' : '#fafafa'
-      }}pre{white-space:pre-wrap;word-wrap:break-word;word-break:break-all}`
+  GM.getResourceUrl(STYLE)
+    .then((url) => fetch(url))
+    .then((resp) => resp.text())
+    .then((style) =>
+      GM_addStyle(
+        `${style}*{margin:0;padding:0}html{line-height:1em;background:${
+          STYLE === 'dark' ? '#282c34' : '#fafafa'
+        }}pre{white-space:pre-wrap;word-wrap:break-word;word-break:break-all}`
+      )
     );
-  };
 
-  const prettier = GM.getResourceUrl('prettier'),
-    parserBabel = GM.getResourceUrl('parser-babel'),
-    parserPostcss = GM.getResourceUrl('parser-postcss'),
-    hljs = GM.getResourceUrl('hljs'),
-    theme = GM.getResourceUrl(STYLE)
-      .then((url) => fetch(url))
-      .then((resp) => resp.text());
+  let source = output.textContent;
 
-  Promise.all([prettier, parser === 'css' ? parserPostcss : parserBabel, hljs, theme]).then((urls) => {
-    worker.postMessage({
-      libs: urls,
-      parser: parser,
-      content: output.textContent,
-    });
-  });
+  try {
+    if (parser) source = prettier.format(source, { parser: parser, plugins: prettierPlugins });
+  } catch (err) {
+    console.error(err);
+  }
+
+  source = hljs.highlightAuto(source).value;
+
+  const fragment = document.createDocumentFragment(),
+    pre = document.createElement('pre');
+
+  pre.innerHTML = source;
+  pre.className = 'hljs';
+
+  fragment.appendChild(pre);
+  document.body.replaceChild(fragment, output);
 })();
