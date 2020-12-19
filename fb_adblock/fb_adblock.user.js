6a7481335bc972635625b4125b58774f43a2b797
lelinhtinh <lelinhtinh2013@gmail.com>
2020-12-19 15:02:13 +0700

    Update Facebook Adblocker

--- fb_adblock/fb_adblock.user.js
+++ fb_adblock/fb_adblock.user.js
@@ -4,7 +4,7 @@
 // @namespace       https://lelinhtinh.github.io
 // @description     Block all ads in Facebook News Feed.
 // @description:vi  Chặn quảng cáo được tài trợ trên trang chủ Facebook.
-// @version         1.2.0
+// @version         1.3.0
 // @icon            https://i.imgur.com/F8ai0jB.png
 // @author          lelinhtinh
 // @oujs:author     baivong
@@ -20,60 +20,96 @@
 (function () {
   'use strict';
 
-  /**
-   * Logging level
-   * @type {Number}
-   */
-  const DEBUG = 0;
-
-  /* === DO NOT CHANGE === */
-  let countAds = 0;
-
-  const config = {
-    attributes: false,
-    childList: true,
-    subtree: true,
-  };
-
-  const removeAds = (wrap, isWatch) => {
-    if (DEBUG >= 2) console.log(wrap, 'wrapNode');
-    if (DEBUG >= 2) console.log(isWatch, 'isWatch');
-
-    const subtitles = wrap.querySelectorAll('[aria-label="Sponsored"], [aria-label="Được tài trợ"]');
-    if (!subtitles.length) return;
-
-    subtitles.forEach((v) => {
-      v = v.closest(isWatch ? '[data-pagelet="MainFeed"]>div>div>div>div' : '[data-pagelet^="FeedUnit"]');
-      if (DEBUG) console.log(++countAds, 'countAds');
-      if (DEBUG >= 3) console.log(v.innerHTML, 'htmlAds');
-      v.remove();
-    });
-  };
-
+  let adsCount = 0;
+  let labelStore = [];
+  let observerLabel;
   let observerStory;
   let observerHead;
+  let isWatch;
+
+  const findAds = (wrapper) => {
+    function removeAds() {
+      if (!labelStore.length) return;
+      const labelId = labelStore.pop();
+
+      const adsLabel = wrapper.querySelector('span[aria-labelledby="' + labelId + '"][class]');
+      if (adsLabel === null) return;
+
+      const adsWrap = adsLabel.closest(isWatch ? 'div:not([class*=" "])' : '[data-pagelet^="FeedUnit"]');
+      adsWrap.remove();
+      // adsWrap.style.opacity = 0.1;
+
+      console.log(++adsCount, 'adsCount');
+      removeAds();
+    }
+    removeAds();
+
+    const watchLabel = (labelHidden) => {
+      if (observerLabel) return;
+      observerLabel = new MutationObserver((mutationsList) => {
+        for (let mutation of mutationsList) {
+          if (
+            mutation.type === 'attributes' &&
+            mutation.attributeName === 'id' &&
+            /(Được\s+tài\s+trợ|Sponsored)/i.test(mutation.target.textContent.trim())
+          ) {
+            labelStore.push(mutation.target.id);
+            removeAds();
+          }
+        }
+      });
+      observerLabel.observe(labelHidden, {
+        attributes: true,
+        attributeFilter: ['id'],
+        subtree: true,
+      });
+    };
+
+    const labelHidden = document.querySelector('[hidden="true"]');
+    if (labelHidden === null) {
+      if (observerLabel) {
+        observerLabel.disconnect();
+        observerLabel = null;
+      }
+    } else {
+      watchLabel(labelHidden);
+    }
+  };
 
   const init = () => {
-    if (DEBUG) console.log('Facebook Adblocker');
-
+    isWatch = location.pathname === '/watch';
     const newsFeed = document.querySelector('[role="feed"], [data-pagelet="MainFeed"]');
-    if (DEBUG >= 2) console.log(newsFeed, 'newsFeedNode');
-    if (!newsFeed) return;
+    if (newsFeed === null) return;
 
     if (observerStory) observerStory.disconnect();
     observerStory = new MutationObserver((mutationsList) => {
       for (let mutation of mutationsList) {
-        removeAds(mutation.target, !newsFeed.getAttribute('role'));
+        findAds(mutation.target);
       }
     });
-    observerStory.observe(newsFeed, config);
+    observerStory.observe(newsFeed, {
+      attributes: false,
+      childList: true,
+      subtree: true,
+    });
 
-    removeAds(document);
+    findAds(document);
   };
 
   init();
 
   if (observerHead) observerHead.disconnect();
   observerHead = new MutationObserver(init);
-  observerHead.observe(document.head, config);
+  observerHead.observe(document.head, {
+    attributes: true,
+    childList: true,
+    subtree: true,
+  });
+
+  (function (old) {
+    window.history.pushState = function () {
+      old.apply(window.history, arguments);
+      init();
+    };
+  })(window.history.pushState);
 })();
