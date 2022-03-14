e32b948d71dee37eb425cd8542f2737d771e0179
Lê Thành <lhthanh.pro@gmail.com>
2022-03-14 10:24:13 +0700

    - gitlab + nestjs docs

--- no_politics/no_politics.user.js
+++ no_politics/no_politics.user.js
@@ -4,12 +4,12 @@
 // @namespace       https://lelinhtinh.github.io
 // @description     Remove political distractions
 // @description:vi  Xóa nhưng phiền nhiễu liên quan đến chính trị
-// @version         1.0.0
+// @version         1.1.0
 // @icon            https://i.imgur.com/24omnOZ.png
 // @author          lelinhtinh
 // @oujs:author     baivong
 // @license         MIT; https://baivong.mit-license.org/license.txt
-// @match           https://gitlab.com/*
+// @match           https://docs.nestjs.com/*
 // @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
 // @noframes
 // @supportURL      https://github.com/lelinhtinh/Userscript/issues
@@ -17,14 +17,6 @@
 // @grant           GM_addStyle
 // ==/UserScript==
 
-GM_addStyle(`#logo {
-  width: calc(28px + 8px * 2);
-  height: calc(28px + 2px * 2);
-  background: url(https://gitlab.com/assets/gitlab_logo-7ae504fe4f68fdebb3c2034e36621930cd36ea87924c11ff65dbcb8ed50dca58.png)
-    no-repeat center center;
-  background-size: cover;
-  background-origin: content-box;
-}
-#logo img {
+GM_addStyle(`.top-bar {
   display: none;
 }`);
