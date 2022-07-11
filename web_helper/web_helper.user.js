19a4081c7267bee51af40ce62938a8fa003de3d7
lelinhtinh <lelinhtinh2013@gmail.com>
2022-07-12 00:15:04 +0700

    Add truyendich

--- web_helper/web_helper.user.js
+++ web_helper/web_helper.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
-// @name            WebHelper
-// @name:vi         WebHelper
+// @name            NovelHelper
+// @name:vi         NovelHelper
 // @namespace       https://lelinhtinh.github.io
-// @description     Add some useful features to some websites.
-// @description:vi  Bổ sung một số tính năng hữu ích cho một vài trang web.
-// @version         1.1.2
+// @description     Add navigation features to some web novels.
+// @description:vi  Bổ sung tính năng điều hướng cho một vài trang web truyện.
+// @version         1.2.0
 // @icon            https://i.imgur.com/FHgT0E4.png
 // @author          Zzbaivong
 // @oujs:author     baivong
 // @license         MIT; https://baivong.mit-license.org/license.txt
 // @match           https://tienhieptruyen.net/*
+// @match           https://truyendich.com/*
 // @require         https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
 // @noframes
 // @supportURL      https://github.com/lelinhtinh/Userscript/issues
@@ -21,38 +22,59 @@
 /* global hotkeys */
 const k = hotkeys.noConflict();
 
-const chapter = document.querySelector('.chapter-c');
+let chapSelector, preSelector, nextSelector;
+switch (location.host) {
+  case 'tienhieptruyen.net':
+    chapSelector = '.chapter-c';
+    preSelector = () => {
+      const preBtn = document.querySelector('.chap-header .btn-next:not([disabled]) .fa-angle-double-left');
+      if (preBtn === null) return;
+      preBtn.parentNode.click();
+    };
+    nextSelector = () => {
+      const nextBtn = document.querySelector('.chap-header .btn-next:not([disabled]) .fa-angle-double-right');
+      if (nextBtn === null) return;
+      nextBtn.parentNode.click();
+    };
+    break;
+  case 'truyendich.com':
+    chapSelector = '#read-content';
+    preSelector = '.btn-chapter-pre:not(.disable)';
+    nextSelector = '.btn-chapter-next:not(.disable)';
+    break;
+  default:
+    console.log('NovelHelper: Unknown host');
+}
+
+const chapter = document.querySelector(chapSelector);
 chapter.scrollIntoView();
 
-document.querySelector('.chap-content').style.userSelect = 'auto';
-chapter.querySelectorAll('[style]').forEach((e) => {
-  e.remove();
-});
-document.addEventListener(
-  'contextmenu',
-  (e) => {
-    e.stopPropagation();
-  },
-  true,
-);
-
-let lineHeight = parseFloat(getComputedStyle(chapter).lineHeight);
-document.querySelector('.chap-list-update .font-size').addEventListener('change', () => {
-  setTimeout(() => {
-    lineHeight = parseFloat(getComputedStyle(chapter).lineHeight);
-  }, 100);
-});
+const navChap = (btnSelector) => {
+  const button = document.querySelector(btnSelector);
+  if (button === null) return;
+  button.click();
+};
 
 k('left', () => {
-  document.querySelector('.chap-header .btn-next .fa-angle-double-left').parentNode.click();
+  if (typeof preSelector === 'string') {
+    navChap(preSelector);
+  } else {
+    preSelector();
+  }
 });
 
 k('right', () => {
-  document.querySelector('.chap-header .btn-next .fa-angle-double-right').parentNode.click();
+  if (typeof nextSelector === 'string') {
+    navChap(nextSelector);
+  } else {
+    nextSelector();
+  }
 });
 
+const contentLineHeight = () => parseFloat(getComputedStyle(chapter).lineHeight);
+
 k('up', () => {
-  document.documentElement.scrollTop -= window.innerHeight - lineHeight * 2;
+  document.documentElement.scrollTop -= window.innerHeight - contentLineHeight() * 2;
 });
 
 let endChapter = false;
@@ -62,11 +84,11 @@ k('down', () => {
     return;
   }
 
-  document.documentElement.scrollTop += window.innerHeight - lineHeight * 2;
+  document.documentElement.scrollTop += window.innerHeight - contentLineHeight() * 2;
 
   const chapterRect = chapter.getBoundingClientRect();
   if (
-    chapterRect.top + chapterRect.height - window.innerHeight + lineHeight * 2 < 0 ||
+    chapterRect.top + chapterRect.height - window.innerHeight + contentLineHeight() * 2 < 0 ||
     document.documentElement.scrollTop + window.innerHeight === document.documentElement.scrollHeight
   ) {
     endChapter = true;
