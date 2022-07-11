1e258c2815ceea7d40f987382001f3a716ec2b46
lelinhtinh <lelinhtinh2013@gmail.com>
2022-07-11 23:21:28 +0700

    Touch navigation support

--- web_helper/web_helper.user.js
+++ web_helper/web_helper.user.js
@@ -4,7 +4,7 @@
 // @namespace       https://lelinhtinh.github.io
 // @description     Add some useful features to some websites.
 // @description:vi  Bổ sung một số tính năng hữu ích cho một vài trang web.
-// @version         1.1.1
+// @version         1.1.2
 // @icon            https://i.imgur.com/FHgT0E4.png
 // @author          Zzbaivong
 // @oujs:author     baivong
@@ -30,8 +30,8 @@ chapter.querySelectorAll('[style]').forEach((e) => {
 });
 document.addEventListener(
   'contextmenu',
-  function (event) {
-    event.stopPropagation();
+  (e) => {
+    e.stopPropagation();
   },
   true,
 );
@@ -43,11 +43,10 @@ document.querySelector('.chap-list-update .font-size').addEventListener('change'
   }, 100);
 });
 
-let endChapter = false;
-
 k('left', () => {
   document.querySelector('.chap-header .btn-next .fa-angle-double-left').parentNode.click();
 });
+
 k('right', () => {
   document.querySelector('.chap-header .btn-next .fa-angle-double-right').parentNode.click();
 });
@@ -55,6 +54,8 @@ k('right', () => {
 k('up', () => {
   document.documentElement.scrollTop -= window.innerHeight - lineHeight * 2;
 });
+
+let endChapter = false;
 k('down', () => {
   if (endChapter) {
     k.trigger('right');
@@ -71,3 +72,15 @@ k('down', () => {
     endChapter = true;
   }
 });
+
+document.addEventListener(
+  'touchstart',
+  (e) => {
+    if (e.changedTouches[0].clientX < (window.innerWidth / 100) * 40) {
+      k.trigger('up');
+    } else {
+      k.trigger('down');
+    }
+  },
+  true,
+);
