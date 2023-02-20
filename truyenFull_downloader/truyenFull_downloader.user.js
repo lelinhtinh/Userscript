44bfe1f3c92189f09481d2bb15a4a5d619f29c58
Lê Thành <lhthanh.pro@gmail.com>
2023-02-20 17:18:54 +0700

    chore: update version

--- truyenFull_downloader/truyenFull_downloader.user.js
+++ truyenFull_downloader/truyenFull_downloader.user.js
@@ -4,16 +4,15 @@
 // @namespace       https://baivong.github.io/
 // @description     Tải truyện từ TruyenFull định dạng EPUB.
 // @description:vi  Tải truyện từ TruyenFull định dạng EPUB.
-// @version         4.6.9
+// @version         4.7.0
 // @icon            https://i.imgur.com/FQY8btq.png
 // @author          Zzbaivong
 // @oujs:author     baivong
 // @license         MIT; https://baivong.mit-license.org/license.txt
-// @match           https://truyenfull.vn/*/
-// @match           https://truyenfull.net/*/
-// @require         https://code.jquery.com/jquery-3.5.1.min.js
+// @match           https://truyenfull.vn/*
+// @require         https://code.jquery.com/jquery-3.6.3.min.js
 // @require         https://unpkg.com/jszip@3.1.5/dist/jszip.min.js
-// @require         https://unpkg.com/file-saver@2.0.2/dist/FileSaver.min.js
+// @require         https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
 // @require         https://unpkg.com/ejs@2.7.4/ejs.min.js
 // @require         https://unpkg.com/jepub@2.1.4/dist/jepub.min.js
 // @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
--- truyenfull-com_downloader/truyenfull-com_downloader.user.js
+++ truyenfull-com_downloader/truyenfull-com_downloader.user.js
@@ -4,7 +4,7 @@
 // @namespace       https://lelinhtinh.github.io/
 // @description     Tải EPUB truyện từ truyenfull.com.
 // @description:vi  Tải EPUB truyện từ truyenfull.com.
-// @version         1.0.0
+// @version         1.1.0
 // @icon            https://i.imgur.com/pn1dLFw.png
 // @author          Zzbaivong
 // @oujs:author     baivong
